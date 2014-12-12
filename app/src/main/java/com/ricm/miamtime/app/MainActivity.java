6b0a4f573eb849b734912d1c9e8349b60841b6f2
Alan <Alan.damotte@gmail.com>
2014-12-12 16:32:07 +0100

    Gestion rayon dans les parametres

--- app/src/main/java/com/ricm/miamtime/app/MainActivity.java
+++ app/src/main/java/com/ricm/miamtime/app/MainActivity.java
@@ -17,11 +17,9 @@ package com.ricm.miamtime.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -69,10 +67,11 @@ public class MainActivity extends ActionBarActivity {
     private void openPreferredLocationInMap() {
         SharedPreferences sharedPrefs =
                 PreferenceManager.getDefaultSharedPreferences(this);
-        String location = sharedPrefs.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
 
+        String range = sharedPrefs.getString(
+                getString(R.string.pref_range_key),
+                getString(R.string.pref_range_default));
+/*
         // Using the URI scheme for showing a location found on a map.  This super-handy
         // intent can is detailed in the "Common Intents" page of Android's developer site:
         // http://developer.android.com/guide/components/intents-common.html#Maps
@@ -87,6 +86,6 @@ public class MainActivity extends ActionBarActivity {
             startActivity(intent);
         } else {
             Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
-        }
+        }*/
     }
 }
---
+++ app/src/main/java/com/ricm/miamtime/app/Utility.java
@@ -0,0 +1,17 @@
+package com.ricm.miamtime.app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by alan on 12/12/14.
+ */
+public class Utility {
+    public static String getPreferredRange(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(context.getString(R.string.pref_range_key),
+                context.getString(R.string.pref_range_default));
+    }
+}
+
